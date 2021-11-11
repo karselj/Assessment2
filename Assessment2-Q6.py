9a13c06741771dc6c5a1baff4d448e2701acbf10
karselj <karolineseljebotn@gmail.com>
2021-11-11 09:24:39 +0100

    Original version

---
+++ Assessment2-Q6.py
@@ -0,0 +1,22 @@
+import random
+
+class RandomNumbers:
+    def __init__(self, amount, minRange, maxRange):
+        self.amount = amount
+        self.minRange = minRange
+        self.maxRange = maxRange
+
+    def generateRandom(self):
+        """Preconditions: amount > 0
+                          minRange < maxRange
+           Postconditions: At least one number is printed to the screen
+                           The number(s) printed to the screen are in the
+                           range minRange to maxRange"""
+        assert self.amount > 0
+        assert self.minRange < self.maxRange
+        for n in range(0, self.amount):
+            num = random.randint(self.minRange, self.maxRange)
+            print(num)
+
+secret = RandomNumbers(1, 1, 10)
+secret.generateRandom()
